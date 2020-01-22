f0dc4fc22f7c018cb687f5dde5f427f9be8e5483
atunsal <alpertuna.unsal@ontariotechu.net>
2020-01-22 15:14:49 -0500

    Fifth Commit

--- lab02/src/main/java/gradleDemo/Demo.java
+++ lab02/src/main/java/gradleDemo/Demo.java
@@ -1,13 +1,6 @@
 package gradleDemo;
-
-import org.apache.commons.validator.routines.*;
-
-public class Demo {
-   static final String cardNum = "4716841076742166";
-
-   public static void main(String[] args) {
-       CreditCardValidator validator = new CreditCardValidator(CreditCardValidator.AMEX + CreditCardValidator.VISA);
-       boolean valid = validator.isValid(cardNum);
-       System.out.println("Is the credit card number valid? " + valid);
-   }
+public class Demo { 
+    public static void main(String[] args) { 
+        System.out.println("Testing 1 2 3..."); 
+    } 
 }
